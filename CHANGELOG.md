363ed12af808748c1364f30dd8dc1054fdf59a81
Damien Seguin <dmnsgn@gmail.com>
2022-06-13 19:37:03 +0100

    chore(release): 2.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+# [2.0.0](https://github.com/dmnsgn/glsl-rotate/compare/v1.1.0...v2.0.0) (2022-06-13)
+
+
+### Code Refactoring
+
+* use ES modules ([cb7dfc9](https://github.com/dmnsgn/glsl-rotate/commit/cb7dfc96a8aab08b5322156ad856e0496790d6b7))
+
+
+### BREAKING CHANGES
+
+* switch to type module
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-export { default as ROTATE, dependencies as ROTATE_DEPENDENCIES } from "./rotate.glsl.js";
+export {
+  default as ROTATE,
+  dependencies as ROTATE_DEPENDENCIES,
+} from "./rotate.glsl.js";
 
-export { default as ROTATE_X, dependencies as ROTATE_X_DEPENDENCIES } from "./rotateX.glsl.js";
+export {
+  default as ROTATE_X,
+  dependencies as ROTATE_X_DEPENDENCIES,
+} from "./rotateX.glsl.js";
 
-export { default as ROTATE_Y, dependencies as ROTATE_Y_DEPENDENCIES } from "./rotateY.glsl.js";
+export {
+  default as ROTATE_Y,
+  dependencies as ROTATE_Y_DEPENDENCIES,
+} from "./rotateY.glsl.js";
 
-export { default as ROTATE_Z, dependencies as ROTATE_Z_DEPENDENCIES } from "./rotateZ.glsl.js";
+export {
+  default as ROTATE_Z,
+  dependencies as ROTATE_Z_DEPENDENCIES,
+} from "./rotateZ.glsl.js";
 
 export { default as ROTATION_2D } from "./rotation-2d.glsl.js";
 
